d6d3f1c4b9fbc7c91112febff28503788863fbe9
Grace Boke <graceboke2019@gmail.com>
2024-09-05 22:07:47 +0300

    my files

---
+++ check.js
@@ -0,0 +1,31 @@
+//**
+// What You're Aiming For
+
+// Section 1: General Awareness
+// Web Development Basics:
+
+// What is web development, and why is it essential in the context of the internet?
+
+web development is the creation of web sites and it is essential because it is widely used in the world to bring people together
+in business and almost all fields.
+// HTML Knowledge:
+
+// Have you heard of HTML? If yes, what does HTML stand for, and what is its primary purpose in web development?
+
+HTML stands for HyperText Markup Language
+purpose of HTML
+it basically made up of elements or tags and attributes which work together to identify document parts and tell the browser 
+how to display them.
+Basically it consist of the structure of thwe web page
+// Section 2: Familiarity with CSS
+// CSS Understanding:
+// Are you familiar with CSS? If yes, briefly explain what CSS is used for in web development.
+
+CSS- Cascading Style Sheet
+CSS is used to style your web page.
+// Section 3: JavaScript and Its Importance
+// Introduction to JavaScript:
+// Have you heard of JavaScript? If yes, what is JavaScript, and why is it crucial for building interactive websites?
+// Section 4: Introduction to React and Node.js 
+//
+
